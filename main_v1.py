8cf1d6c5737002d2f76b3aaa77bda1c3c2c1413e
BeardedBart <apfsbrt@gmail.com>
2023-03-15 14:40:04 +0100

    Added code

--- main_v1.py
+++ main_v1.py
@@ -1,8 +1,26 @@
 # NOTE first steps of creating an alghoritm
 import numpy as np
 
+# initialize array for calculations
 idx = input("How much steps?: ")
-
 array = np.zeros((4, int(idx)))
-array[1, 1] = 1
+array[1, 1] *= 5
+
+
+def drag(rho, cd, V, S):
+    '''Calculate drag'''
+    return 0.5*rho*cd*(V**2)*S
+
+
+D = drag(1.225, 0.1, 10, 2)
+
+
+def first_column():
+    None
+
+
+def nth_column():
+    None
+
+
 print(array)
