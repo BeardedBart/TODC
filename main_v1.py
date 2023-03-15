fdb79b1896389468de02df06df53b78c3d25431d
BeardedBart <apfsbrt@gmail.com>
2023-03-15 20:31:52 +0100

    Started working on project. Load_config func created, some tests and added config file in JSON format

--- main_v1.py
+++ main_v1.py
@@ -1,18 +1,28 @@
 # NOTE first steps of creating an alghoritm
 import numpy as np
+import json
 
 # initialize array for calculations
-idx = input("How much steps?: ")
-array = np.zeros((4, int(idx)))
-array[1, 1] *= 5
+array = np.zeros((4, 2))
+
+
+def load_config():
+    initData = dict()
+    with open('config.json', 'r', encoding='utf-8') as f:
+        initData = json.load(f)
+
+    T = list(initData['thrust'])
+    V = float(initData['stall_speed']) * 1.1
+    S = float(initData['wing_area'])
+    M = float(initData['mass'])
+    CD = float(initData['aerodynamic_drag_coeff'])
+    WD = float(initData['wheel_drag'])
+    return T, V, CD, WD, S, M
 
 
 def drag(rho, cd, V, S):
     '''Calculate drag'''
     return 0.5*rho*cd*(V**2)*S
-
-
-D = drag(1.225, 0.1, 10, 2)
 
 
 def first_column():
@@ -23,4 +33,7 @@ def nth_column():
     None
 
 
-print(array)
+if __name__ == '__main__':
+    data = load_config()
+    d = drag(1.225, data[2], data[1], data[4])
+    print(d)
